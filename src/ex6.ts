39ef2e09901387083e342b86092c2addb4dcf65e
Nguyen Nhat Quan <NguyenNhatQuan06022004@gmail.com>
2025-08-27 08:05:26 +0700

    bai 6

---
+++ src/ex6.ts
@@ -0,0 +1,13 @@
+// 6. Use Promise.all() to run 3 simulated Promises in parallel and print the result.
+
+const Task = function (time : number,mess : string) : Promise<string> {
+    return new Promise(reslove => {
+        setTimeout(()=> reslove(mess), time)
+    })
+}
+
+const Task1 = Task(1000,"Task 1 done")
+const Task2 = Task(2000,"Task 2 done")
+const Task3 = Task(3000,"Task 3 done")
+
+Promise.all([Task1,Task2,Task3]).then((res : string[]) => console.log(res) )
