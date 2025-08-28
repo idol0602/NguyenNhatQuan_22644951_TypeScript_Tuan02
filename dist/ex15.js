112ae9842c80ef78f81551012c21555ea0f58e25
Nguyen Nhat Quan <NguyenNhatQuan06022004@gmail.com>
2025-08-28 22:17:32 +0700

    bai 15

---
+++ dist/ex15.js
@@ -0,0 +1,19 @@
+"use strict";
+// 15. Call multiple async functions sequentially using await.
+async function multiplyAfterDelay(x) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(x * 2);
+        }, 1000);
+    });
+}
+async function processSequential() {
+    const first = await multiplyAfterDelay(5);
+    console.log("First =", first);
+    const second = await multiplyAfterDelay(10);
+    console.log("Second =", second);
+    const third = await multiplyAfterDelay(15);
+    console.log("Third =", third);
+    console.log("Sum =", first + second + third);
+}
+processSequential();
