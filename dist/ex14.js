4432a948c8d10905a9cce38d1e7cd9c07d3637f9
Nguyen Nhat Quan <NguyenNhatQuan06022004@gmail.com>
2025-08-28 22:15:36 +0700

    bai 14

---
+++ dist/ex14.js
@@ -0,0 +1,13 @@
+"use strict";
+// 14. Write an async function that takes a number, waits 1 second, and returns the number × 3.
+async function tripleAfter1s(num) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(num * 3);
+        }, 1000);
+    });
+}
+(async () => {
+    const result = await tripleAfter1s(5);
+    console.log(result);
+})();
